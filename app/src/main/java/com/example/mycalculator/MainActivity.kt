a967d9491e2b6189d02f0db7e3be9e0482330c95
Gopal kumar mishra <20gopalkmishra@gmail.com>
2023-07-14 01:42:20 +0530

    FIRST COMMIT

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -13,6 +13,8 @@ class MainActivity : AppCompatActivity() {
 
     private var ramji:TextView?=null
     private var seven:Button?=null
+    var lastnumeric:Boolean=false
+    var lastdot:Boolean=false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,13 +24,99 @@ class MainActivity : AppCompatActivity() {
     }
     fun myname(view: View){
         ramji?.append((view as Button).text)
+        lastnumeric=true
+        lastdot=false
 
     }
-    //fun onclear(view: View){
-      //  ramji?.text=""
-   // }
+
 
     fun onclear(view: View){
         ramji?.text=""
+    }
+
+    fun onDecimalpoint(view: View){
+        ramji?.text?.let{
+        if(lastnumeric  && !lastdot ){
+            if(abc(it.toString())){
+            ramji?.append(".")
+            lastnumeric=false
+            lastdot=true}
+        }
+
+    }}
+    private  fun abc(value: String):Boolean {
+        return if (value.contains(".")){
+            false
+        }else{
+            true
+        }
+    }
+     fun onoperator(view: View){
+        ramji?.text?.let{
+            if(lastnumeric && !isoperatoradded(it.toString()))
+                ramji?.append((view as Button).text)
+            lastnumeric=false
+            lastdot=false
+        }
+    }
+
+
+   private fun isoperatoradded(value : String):Boolean{
+        return if(value.startsWith("-")){
+            false }
+       else{value.contains("+")||value.contains("-")||
+                value.contains("*")||value.contains("/")
+
+        }
+    }
+    fun onequal(view: View){
+
+        if(lastnumeric){
+            var tvValue=ramji?.text.toString()
+            var prefix=""
+            try {
+                if (tvValue.startsWith("-")) {
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+                }
+                if (tvValue.contains("-")) {
+                    val splitValue = tvValue.split("-")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if (prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+                    ramji?.text = (one.toDouble() - two.toDouble()).toString()
+                }else   if (tvValue.contains("+")) {
+                    val splitValue = tvValue.split("+")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if (prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+                    ramji?.text = (one.toDouble() + two.toDouble()).toString()
+                }else   if (tvValue.contains("*")) {
+                    val splitValue = tvValue.split("*")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if (prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+                    ramji?.text = (one.toDouble() * two.toDouble()).toString()
+                }else    if (tvValue.contains("/")) {
+                    val splitValue = tvValue.split("/")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if (prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+                    ramji?.text = (one.toDouble() /two.toDouble()).toString()
+                }
+            }
+        catch (e: ArithmeticException) {
+//hellogi
+        ramji?.append(" ")}
+
+        }
     }
 }
